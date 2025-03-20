04c82576e8e2164fc9eb3f65d6511b023fefc8cd
28weisnerb <28weisnerb@bwschools.net>
2025-03-20 08:42:30 -0400

    c

--- main.py
+++ main.py
@@ -8,39 +8,86 @@ t = turtle.Turtle()
 t.penup()
 t.goto(0,200)
 t.pendown()
-t.pencolor('blue')
-t.write("Background Color",font=("Arial",20,"bold"),align="center")
+t.pencolor('white')
+t.write("Background Color",font=("Arial",30,"bold"),align="center")
 t.penup()
-t.goto(0,100)
+t.goto(-75,100)
 t.pendown()
-t.pencolor('blue')
+t.pencolor('tan')
 t.write("1. Tan",font=("Arial",20,"bold"),align="left")
 t.speed(0)
 t.penup()
-t.goto(0,50)
+t.goto(-75,50)
 t.pendown()
-t.pencolor('blue')
+t.pencolor('azure')
 t.write("2. Azure",font=("Arial",20,"bold"),align="left")
+t.penup()
+t.goto(-75,0)
+t.pendown()
+t.pencolor('aquamarine')
+t.write("3. Aqua Marine",font=("Arial",20,"bold"),align="left")
+t.penup()
+t.goto(-75,-50)
+t.pendown()
+t.pencolor('darkkhaki')
+t.write("4. Dark Khaki",font=("Arial",20,"bold"),align="left")
+t.penup()
+t.goto(0,-150)
+t.pendown()
+t.pencolor('white')
+t.write("Choose a Color",font=("Arial",30,"bold"),align="center")
+screen.bgcolor('black')
+
+choose=int(input("Choose your color: "))
+if choose == 1:
+    screen.bgcolor('tan')
+elif choose == 2:
+    screen.bgcolor('azure')
+elif choose == 3:
+    screen.bgcolor('aquamarine')
+else:
+    screen.bgcolor('darkkhaki')
+t.clear()
 t.penup()
 t.goto(0,0)
 t.pendown()
-t.pencolor('blue')
-t.write("3. Cornsilk",font=("Arial",20,"bold"),align="left")
-t.penup()
-t.goto(0,-50)
-t.pendown()
-t.pencolor('blue')
-t.write("4. Khaki",font=("Arial",20,"bold"),align="left")
-screen.bgcolor('tan')
+t.pencolor('black')
+t.write("Enter Your Name",font=("Arial",30,"normal"),align="center")
+
 
 name= input("Enter Your Name: ")
+t.clear()
 # num1=int(input("Enter a number: "))
 # num2=int(input("Enter another number: "))
 # sum=int(input(num1 + num2))
-num1= random.randint(-100,100)
-num2= random.randint(-100,100)
-sum=num1+num2
+operation = random.randint(1,4)
+if operation == 1:
+    symbol = "+"
+    num1= random.randint(-100,100)
+    num2= random.randint(-100,100)
 
+    solution = num1+num2
+elif operation == 2:
+    symbol = "-"
+    num1 = random.randint(-100, 100)
+    num2 = random.randint(-100, 100)
+
+    solution = num1 - num2
+elif operation == 3:
+    symbol = "*"
+    num1 = random.randint(-10, 10)
+    num2 = random.randint(-10, 10)
+
+    solution = num1 * num2
+elif operation == 4:
+    symbol = "/"
+    num1 = random.randint(-10, 10)
+    num2 = random.randint(1, 10)
+    sign= random.randint(1,2)
+    if sign == 2:
+        num2= -2*num2
+
+    solution = num1 / num2
 t.penup()
 t.goto(0,100)
 t.pendown()
@@ -63,7 +110,7 @@ t.penup()
 t.goto(-100,0)
 t.pendown()
 t.pencolor('blue')
-t.write("+",font=("Arial",30,"bold"),align="center")
+t.write(symbol,font=("Arial",30,"bold"),align="center")
 
 t.penup()
 t.goto(100,0)
@@ -71,14 +118,14 @@ t.pendown()
 t.pencolor('red')
 t.write("=",font=("Arial",30,"bold"),align="center")
 
-ans=int(input("Please enter your answer: "))
+ans=float(input("Please enter your answer: "))
 
 
 t.penup()
 t.goto(200,0)
 t.pendown()
 t.pencolor('red')
-t.write(sum,font=("Arial",30,"bold"),align="center")
+t.write(solution,font=("Arial",30,"bold"),align="center")
 
 
 
@@ -86,19 +133,23 @@ t.penup()
 t.goto(0,-100)
 t.pendown()
 t.pencolor('black')
-t.write("Your answer is: ",font=("Arial",30,"bold"),align="center")
+t.write("Your answer is: "+str(solution),font=("Arial",30,"bold"),align="center")
 
-if ans == sum:
+if ans == solution:
     screen.bgcolor('dodgerblue')
     t.penup()
     t.goto(0, 50)
     t.pendown()
     t.pencolor('green')
-    t.write("Correct " + str(ans), font=("Arial", 30, "bold"), align="center")
+    t.write("Correct", font=("Arial", 30, "bold"), align="center")
 
 else:
     screen.bgcolor('darkorange')
-
+    t.penup()
+    t.goto(0, 50)
+    t.pendown()
+    t.pencolor('red')
+    t.write("Incorrect: " + str(ans), font=("Arial", 30, "bold"), align="center")
 
 
 turtle.done()
